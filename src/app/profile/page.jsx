02bad4a7a63f33014b986782d72ab5e0cf735feb
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-04-28 22:53:28 +0600

    proxy added

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,12 +2,14 @@
 import { UpdateUserInfo } from '@/components/UpdateUserInfo';
 import { authClient } from '@/lib/auth-client';
 import { Avatar, Card } from '@heroui/react';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
 const ProfilePage = () => {
     const userData = authClient.useSession();
     const user = userData.data?.user;
     console.log(user);
+
 
     return (
         <div className='my-10 w-[95%] md:w-[85%] mx-auto'>
@@ -17,7 +19,7 @@ const ProfilePage = () => {
                     <Avatar.Fallback>{user?.name[0]}</Avatar.Fallback>
                 </Avatar>
                 <h2 className='text-xl font-bold'>{user?.name}</h2>
-                <p>{user.email}</p>
+                <p>{user?.email}</p>
                 <UpdateUserInfo />
             </Card>
         </div>
